e6f7b0ed43514b42c3aafbb5a1a28cdda99b3163
Hesam Zarnooshe <javad.zarnooshe@gmail.com>
2023-02-24 16:49:35 +0330

    Screen Orientation Locked on Portrait !

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,29 @@
 import 'package:flutter/material.dart';
-import 'package:notepad/pages/welcome_page.dart';
+import 'package:notepad/pages/first_page.dart';
+import 'package:flutter/services.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]).then((value) => runApp(const MyApp()));
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
       title: 'NotePad',
-      home: HomePage(),
+      home: FirstPage(),
       debugShowCheckedModeBanner: false,
     );
   }
 }
 
-
+// FirstImage() For Final App
+// HomePage() For Welcome Page
+// FirstPage() For First Page
